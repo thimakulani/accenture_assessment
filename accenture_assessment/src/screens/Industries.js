a527f76cd2d9051143eb3ecdea0937ceec098e6e
Thima <50391673+thimakulani@users.noreply.github.com>
2023-11-22 18:23:49 +0200

    initial commit

--- accenture_assessment/src/screens/Industries.js
+++ accenture_assessment/src/screens/Industries.js
@@ -13,7 +13,7 @@ const Industries =()=>{
     }, []);
 
     const handleSort = () => {
-        // Sort the data by a specific property (e.g., 'name')
+        // Sort the data by a specific property
         const sortedData = [...brands].sort((a, b) => a.name.localeCompare(b.name));
         setBrands(sortedData);
         setSorted(true);
@@ -28,25 +28,6 @@ const Industries =()=>{
         setFilteredData(filtered);
     };
 
-    const imagePath = require('../assets/visa-black.svg').default;
-    console.log('the path: ' + imagePath);
-    const brnds = [
-        { "id": 1, "name": "visa-black", "icon": null },
-        { "id": 2, "name": "tyme-bank-black", "icon": null},
-        { "id": 3, "name": "distell-black", "icon": null },
-        { "id": 4, "name": "spotify-black", "icon": null },
-        { "id": 5, "name": "microsoft-black", "icon": null },
-        { "id": 6, "name": "engen-black", "icon": null },
-        { "id": 7, "name": "engen-black", "icon": null },
-        { "id": 8, "name": "nike-black", "icon": null },
-        { "id": 9, "name": "wesgrow-black", "icon": null },
-        { "id": 10, "name": "multichoice-black", "icon": null },
-        { "id": 11, "name": "pnp-black", "icon": null },
-        { "id": 12, "name": "liquid-black", "icon": null },
-        { "id": 13, "name": "tfg-black", "icon": null },
-        { "id": 14, "name": "sanlam-black", "icon": null },
-        { "id": 15, "name": "santam-black", "icon": null }
-    ];
 
     //const imagePath = require('../assets/visa-black.svg').default;
     const get_file = (name) => {
@@ -78,7 +59,7 @@ const Industries =()=>{
             <section>
                 <div className="brand-list-container">
                     <div className="brand-list">
-                        {brnds.map(brand => (
+                        {brands.map(brand => (
                             <div key={brand.id} className="brand-item">
                                 <img src={get_file(brand.name)} alt={brand.name} />
                             </div>
